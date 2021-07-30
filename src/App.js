41b5231b3dd4d51b2d6fb6fbd89e3067f780d090
testobesto <barelasaf@yahoo.com>
2021-07-30 18:51:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1>Testo is always besto</h1>
-      <p>Do not forget please</p>
+      <h1>Testo is always best</h1>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
